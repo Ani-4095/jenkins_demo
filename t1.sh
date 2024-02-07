2f9a1673ba417deb807c88301826499eaad51cb5
Ani-4095 <158203163+Ani-4095@users.noreply.github.com>
2024-02-07 23:35:27 +0530

    Update t1.sh

--- t1.sh
+++ t1.sh
@@ -1,1 +1,7 @@
-echo "hii good after noon"
+space=`df -kh | head -5 | tail -1 | awk -F " " '{print$(NF-1)}' | sed 's/%//g'`
+echo $space
+if [ $space >= 80 ]
+then 
+echo "send email" 
+else 
+echo "happy"
